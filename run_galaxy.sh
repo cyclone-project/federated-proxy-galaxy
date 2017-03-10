df9d4e6ac0a34ff61a715badaecb26a82c70e861
root <root@134.158.75.184>
2017-03-10 15:25:56 +0000

    ADD configuration nginx with remote_user mod, folder should by mount in galaxy-docker images. ADD script to run Galaxy instance with right docker command line.

--- run_galaxy.sh
+++ run_galaxy.sh
@@ -1,37 +1,49 @@
-#! /bin/bash 
+#! /bin/bash
 
 NAME=ifbgalaxy
+VERSION=16.10
 PORT=8080
+USER=$USER
 
 
-if [ $(docker ps -a | grep -c galaxy-store) -eq 0 ]; then
-	echo "Create galaxy volume"
-	docker create -v /export --name galaxy-store  bgruening/galaxy-stable  /bin/true
-else
-	echo "Galaxy volume already exists."
-fi
+# if [ $(docker ps -a | grep -c galaxy-store) -eq 0 ]; then
+#         echo "Create galaxy volume"
+#         docker create -v /export --name galaxy-store  bgruening/galaxy-stable:$VERSION  /bin/true
+# else
+#         echo "Galaxy volume already exists."
+# fi
 
 docker stop $NAME
 docker rm -v $NAME
 
-#	--net=host \
-#	--privileged=true \
-#	-e DOCKER_PARENT=True \
-#    	-v /var/run/docker.sock:/var/run/docker.sock \
+#       --net=host \
+#       --privileged=true \
+#       -e DOCKER_PARENT=True \
+#       -v /var/run/docker.sock:/var/run/docker.sock \
 echo "Run galaxy-docker "
 docker run -d \
-	-p $PORT:80 -p 8021:21 -p 8800:8800 \
-	-v /var/log/galaxy-docker:/home/galaxy/logs \
+        -p $PORT:80 -p 8021:21 -p 8800:8800 \
+        -v /root/conf.d:/etc/nginx/conf.d:ro \
 	-e GALAXY_CONFIG_SUPPORT_URL=https://cloud.france-bioinformatique.fr/cloud/help/ \
-	-e GALAXY_CONFIG_USE_REMOTE_USER=True \
-	-e GALAXY_CONFIG_REQUIRE_LOGIN=True \
-	-e GALAXY_CONFIG_FTP_UPLOAD_DIR=/ifb/galaxy/database/files \
-	-e GALAXY_CONFIG_FTP_UPLOAD_SITE=cloud.france-bioinformatique.fr \
-	--volumes-from galaxy-store \
-	--name $NAME \
-	bgruening/galaxy-stable
+        -e GALAXY_CONFIG_USE_REMOTE_USER=True \
+	-e GALAXY_DEFAULT_ADMIN_USER=$USER \
+	-e GALAXY_CONFIG_ADMIN_USERS=$USER \
+        --name $NAME \
+        bgruening/galaxy-stable:$VERSION
+
+	#-e GALAXY_LOGGING=full \
+
+#        -v /root/data-galaxy:/export \
+#        -e GALAXY_CONFIG_FTP_UPLOAD_SITE=cloud.france-bioinformatique.fr \
+#        -e GALAXY_CONFIG_REQUIRE_LOGIN=True \
+#        -e GALAXY_CONFIG_FTP_UPLOAD_DIR=/root/data-galaxy/database/files \
+#        --volumes-from galaxy-store \
+#        -v /var/log/galaxy-docker:/home/galaxy/logs \
 
 echo -e "\n\n"
 sleep 5s
 docker ps
+
+echo -e "\n\n"
+docker logs -f $NAME
 
