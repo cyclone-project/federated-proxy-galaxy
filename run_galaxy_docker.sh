fdd3dc868e2e9bf351beb209f6d7902b9a1f8308
Sandrine Perrin <sandrine.perrin@france-bioinformatique.fr>
2017-03-16 17:20:42 +0100

    ADD example on command line to docker Galaxy images (https://github.com/bgruening/docker-galaxy-stable).

---
+++ run_galaxy_docker.sh
@@ -0,0 +1,28 @@
+#! /bin/bash
+
+NAME=ifbgalaxy
+VERSION=16.10
+ADMIN_USER=$ADMIN_USER
+
+# IMP sous Ubuntu 14.04 docker should have daemon option ----storage-driver=devicemapper
+
+docker stop $NAME
+docker rm -v $NAME
+
+echo "Run galaxy-docker "
+docker run -d \
+        -p 8080:80 -p 8021:21 -p 8800:8800 \
+        -v /root/conf.d:/etc/nginx/conf.d:ro \
+        -e GALAXY_CONFIG_USE_REMOTE_ADMIN_USER=True \
+	-e GALAXY_DEFAULT_ADMIN_ADMIN_USER=$ADMIN_USER \
+	-e GALAXY_CONFIG_ADMIN_ADMIN_USERS=$ADMIN_USER \
+        --name $NAME \
+        bgruening/galaxy-stable:$VERSION
+
+echo -e "\n\n"
+sleep 5s
+docker ps
+
+echo -e "\n\n"
+docker logs -f $NAME
+
